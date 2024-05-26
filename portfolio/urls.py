469a71e530d2cb1301d56101624c9557c2d05476
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-26 23:37:36 +0100

    Ajout de la page de détails du projet et amélioration de la page d'accueil
    - Modification du template de la page d'accueil pour afficher les 100 premiers caractères de la description des projets
    - Création d'une vue et d'un template pour afficher les détails complets de chaque projet
    - Ajout de liens vers les pages de détails des projets depuis la page d'accueil
    - Configuration des URLs pour la page d'accueil et les pages de détails des projets

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,5 @@ app_name = "portfolio"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("project/<int:project_id>/", views.project_detail, name="project_detail"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Project
 
@@ -11,3 +11,8 @@ def home(request):
     projects = paginator.get_page(page_number)
 
     return render(request, "portfolio/home.html", {"projects": projects})
+
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    return render(request, "portfolio/project_detail.html", {"project": project})
