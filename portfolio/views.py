f93b0113740af52631677145e0eaeb317481b143
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-26 23:28:24 +0100

    Amélioration du template de la page d'accueil et ajout de la pagination
    - Ajout de la pagination dans la vue home pour afficher 5 projets par page
    - Utilisation de Bootstrap pour améliorer la mise en page des projets
    - Affichage des projets dans des cartes Bootstrap avec image, titre, description, lien de démo et lien du code source
    - Ajout de la pagination avec Bootstrap pour naviguer entre les pages de projets

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Project
 
 
 def home(request):
-    projects = Project.objects.all()
+    project_list = Project.objects.all()
+    paginator = Paginator(project_list, 5)  # 5 projets par page
+
+    page_number = request.GET.get("page")
+    projects = paginator.get_page(page_number)
+
     return render(request, "portfolio/home.html", {"projects": projects})
