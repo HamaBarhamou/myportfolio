060791b2ff06f32d6bbe75e22cbf182a371003ac
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-26 23:23:15 +0100

    Ajout de la gestion des projets dans l'administration Django
    - Enregistrement du modèle Project dans l'interface d'administration
    - Configuration de l'affichage des champs title, date_created, demo_link et source_code_link dans la liste d'administration
    - Ajout de la fonctionnalité de recherche par title et description dans l'interface d'administration

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "date_created", "demo_link", "source_code_link")
+    search_fields = ("title", "description")
