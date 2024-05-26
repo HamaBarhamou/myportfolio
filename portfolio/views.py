417998921039bd1ae0661ce6db239e03de359e77
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-27 00:03:37 +0100

    Amélioration de la présentation du template de la page d'accueil avec des tailles uniformes pour les projets
    - Utilisation de classes CSS et Flexbox pour s'assurer que les cartes des projets ont une hauteur uniforme
    - Limitation de la hauteur des images à 200px et utilisation de object-fit: cover pour les images
    - Utilisation du filtre truncatechars pour limiter la description à 100 caractères
    - Placement du bouton View Details en bas de la carte pour une présentation uniforme

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,7 +5,7 @@ from .models import Project
 
 def home(request):
     project_list = Project.objects.all()
-    paginator = Paginator(project_list, 5)  # 5 projets par page
+    paginator = Paginator(project_list, 6)  # 5 projets par page
 
     page_number = request.GET.get("page")
     projects = paginator.get_page(page_number)
