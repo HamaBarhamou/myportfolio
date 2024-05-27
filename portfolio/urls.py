e27459fa68bf83b5c416abb5f372639198ad71bb
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-27 22:23:03 +0100

    configuration a about comme etant la premiere page d'acceuil

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "portfolio"
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
     path("project/<int:project_id>/", views.project_detail, name="project_detail"),
-    path("about/", views.about, name="about"),
+    path("", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("events/", views.event_list, name="event_list"),
     path("events/<int:event_id>/", views.event_detail, name="event_detail"),
