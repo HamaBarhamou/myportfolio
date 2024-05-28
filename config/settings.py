0073eddba328c26861bff9f9a2f4f1da1eeab56b
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-28 15:16:42 +0100

    Affichager uniquement des 0 et 1 dans la matrix

--- config/settings.py
+++ config/settings.py
@@ -145,9 +145,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+
 CKEDITOR_UPLOAD_PATH = "uploads/"
-
-
 CKEDITOR_CONFIGS = {
     "default": {
         "toolbar": "full",
